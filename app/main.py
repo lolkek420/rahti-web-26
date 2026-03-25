f1669644b5acd66f9cd6a721b22ecf995f28db44
lolkek420 <dakar13207@gmail.com>
2026-03-25 15:31:52 +0200

    Add IP retrieval endpoint and fix item_id parameter in read_item function

--- app/main.py
+++ app/main.py
@@ -6,7 +6,11 @@ app = FastAPI()
 def read_root():
     return { "msg": "Hello, Dainis!" }
 
+@app.get("/api/ip")
+async def get_ip(request: Request):
+    client_host = request.client.host
+    return {"ip": client_host}
 
-@app.get("/items/{id}")
+@app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
-    return {"id": id, "q": q}
+    return {"id": item_id, "q": q}
