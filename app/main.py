f72d252b706524283c44362b407f66af98c422fd
lolkek420 <dakar13207@gmail.com>
2026-03-31 11:50:02 +0300

    Add CORS middleware and room API endpoints; create HTTP request examples

--- app/main.py
+++ app/main.py
@@ -1,12 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return { "msg": "Hello, Dainis!" }
+# Настройки CORS (как в твоем примере)
+origins = ["*"] 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+rooms = [
+    {"room_number": 101, "type": "Single", "price": 50, "is_booked": False},
+    {"room_number": 102, "type": "Double", "price": 80, "is_booked": True},
+    {"room_number": 201, "type": "Suite", "price": 150, "is_booked": False}
+]
 
 
-@app.get("/items/{id}")
-def read_item(item_id: int, q: str = None):
-    return {"id": id, "q": q}
+@app.get("/api/ip")
+def api_ip(request: Request): 
+    return { "ip": request.client.host }
+
+@app.get("/rooms")
+def get_rooms():
+    return rooms
